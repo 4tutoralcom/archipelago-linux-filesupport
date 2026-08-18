9e688402e2357ddc5e5c48a572e18fc4bd9545e5
Andrew Dittmann <4tutoralcom@gmail.com>
2026-08-17 22:37:52 -0500

    fixed and issue with archipeligo server not loading with space in file path

--- bin/archipelago-server.sh
+++ bin/archipelago-server.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 FILE="$1"
+FILE="${1#\'}"
+FILE="${FILE%\'}"
 
 if [ -z "$FILE" ]; then
-  echo "No .archipelago file provided"
-  exit 1
+    echo "No .archipelago file provided"
+    exit 1
 fi
 
 "${HOME}/AppImages/archipelago.appimage" "$FILE"
