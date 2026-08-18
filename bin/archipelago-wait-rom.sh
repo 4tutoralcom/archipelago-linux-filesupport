6d5ad60434681507afbc8456e230037894bc4320
Andrew Dittmann <4tutoralcom@gmail.com>
2026-08-17 22:36:13 -0500

    update to fix spacs in file path not loading

--- bin/archipelago-wait-rom.sh
+++ bin/archipelago-wait-rom.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 FILE="$1"
+FILE="${1#\'}"
+FILE="${FILE%\'}"
+echo "Opening $FILE"
+
 SCRIPT_DIR=$(readlink -f "${HOME}/bin/pack_scripts")
 
 echo "${SCRIPT_DIR}"
@@ -8,39 +12,40 @@ echo "${SCRIPT_DIR}"
 APP=$(readlink -f "${HOME}/AppImages/archipelago.appimage")
 echo "Starting Archipelago..."
 if [ -z "$FILE" ]; then
-  env DESKTOPINTEGRATION=1 "${APP}"
-  sleep infinity
-  exit
+    env DESKTOPINTEGRATION=1 "${APP}"
+    sleep infinity
+    exit
 fi
 extension="${FILE##*.}"
 ADITIONAL_SCRIPT="${SCRIPT_DIR}/${extension}.sh"
 echo "$ADITIONAL_SCRIPT"
 if [ -f "${SCRIPT_DIR}/${extension}.sh" ]; then
-  "$ADITIONAL_SCRIPT"
+    "$ADITIONAL_SCRIPT"
 fi
 
 # Run and capture output
 env DESKTOPINTEGRATION=1 "$APP" "$FILE" 2>&1 | while IFS= read -r line; do
 
-echo "$line"
 
-  # Detect ROM write line
-  if echo "$line" | grep -q "wrote rom file to"; then
-    ROM_PATH=$(echo "$line" | sed -n 's/.*wrote rom file to //p')
-    
-    # Clean quotes if any
-    ROM_PATH=$(echo "$ROM_PATH" | tr -d '\r"')
+    echo "$line"
 
-    echo ""
-    echo "ROM detected: $ROM_PATH"
-    echo "Launching ROM..."
+    # Detect ROM write line
+    if echo "$line" | grep -q "wrote rom file to"; then
+        ROM_PATH=$(echo "$line" | sed -n 's/.*wrote rom file to //p')
 
-    if [ "${extension}" = "apfirered" ]; then
-      "${HOME}/Applications/BizHawk-2.11.1-linux-x64/EmuHawkMono.sh" "${ROM_PATH}" --lua= "${HOME}/.local/share/Archipelago/data/lua/connector_bizhawk_generic.lua"
-    else
-      xdg-open "$ROM_PATH"
+        # Clean quotes if any
+        ROM_PATH=$(echo "$ROM_PATH" | tr -d '\r"')
+
+        echo ""
+        echo "ROM detected: $ROM_PATH"
+        echo "Launching ROM..."
+
+        if [ "${extension}" = "apfirered" ]; then
+            "${HOME}/Applications/BizHawk-2.11.1-linux-x64/EmuHawkMono.sh" "${ROM_PATH}" --lua= "${HOME}/.local/share/Archipelago/data/lua/connector_bizhawk_generic.lua"
+        else
+            xdg-open "$ROM_PATH"
+        fi
+
     fi
-
-  fi
 
 done
